fa87de6f58d78a7439ec246a8570d9d67c03a774
BeniSetiawan202013025 <80757549+BeniSetiawan202013025@users.noreply.github.com>
2021-07-11 20:38:34 +0700

    Add files via upload

---
+++ latihan_login/src/latihan_login/Latihan_login.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_login;
+
+/**
+ *
+ * @author ACER A314-41
+ */
+public class Latihan_login {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
